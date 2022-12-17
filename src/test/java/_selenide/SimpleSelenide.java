3f803c4042a6ab48fb6b78ea9687389ba0cd6bc3
Reanold Piatrenka <reanold@oktopost.com>
2022-12-17 22:11:58 +0200

    Fix credentials for selenide

--- src/test/java/_selenide/SimpleSelenide.java
+++ src/test/java/_selenide/SimpleSelenide.java
@@ -4,20 +4,26 @@ import static com.codeborne.selenide.Selenide.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import utils.GetCredentials;
 
 import java.util.Objects;
 
 public class SimpleSelenide {
     int defaultTimeout;
 
+    GetCredentials credentials = new GetCredentials("src/main/java/test_data/default_app_user_credentials");
+
     @Test
     public void seleniumAutomation() {
 
+        String userLogin = credentials.getProperty("Username");
+        String userPassword = credentials.getProperty("Password");
+
         open("https://app.oktopost.com");
         $(By.xpath(".//input[@placeholder='name@company.com']")).
-                sendKeys("reanold@oktopost.com");
+                sendKeys(userLogin);
         $(By.xpath(".//input[@placeholder='Password']"))
-                .sendKeys("Reanold_123");
+                .sendKeys(userPassword);
         $(By.xpath(".//*[contains(text(),'Sign in')]")).click();
 
         for (defaultTimeout = 10000; defaultTimeout > 0; defaultTimeout -= 1) {
